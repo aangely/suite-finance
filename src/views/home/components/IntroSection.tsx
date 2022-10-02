85c9009fe76fc9d7a4d7f3f1d1a4e67c210c8796
aangely <angel.yang@crypto.com>
2022-10-02 15:16:54 +0800

    feat: update fonts

--- src/views/home/components/IntroSection.tsx
+++ src/views/home/components/IntroSection.tsx
@@ -3,7 +3,13 @@ import { Container, Heading, Text, VStack } from "@chakra-ui/react";
 export const IntroSection = () => {
   return (
     <VStack alignItems="flex-start" spacing={5}>
-      <Heading fontSize="66" fontFamily="Encode Sans" bgGradient="linear(to-br, #D9475A 16.13%, #F8AED9 47.68%)" bgClip="text">
+      <Heading
+        fontSize="66"
+        fontFamily="Encode Sans"
+        fontWeight="500"
+        bgGradient="linear(to-br, #D9475A 16.13%, #F8AED9 47.68%)"
+        bgClip="text"
+      >
         Suite
       </Heading>
       <Text>
