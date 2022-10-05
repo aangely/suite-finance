624256463cd9b3f0ae92547e2defea9910ade5f1
hengwangsay <hengwangsay@users.noreply.github.com>
2022-10-05 03:14:18 +0000

    deploy: a4bb64856152e01cd15e7a9cf5f6138a125abe13

---
+++ _next/static/chunks/0f81c168-5f1454748ec88d26.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[117],{5967:function(t,n,r){r.d(n,{XhU:function(){return o}});var e=r(9912);function o(t){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M20 12H4"}}]})(t)}}}]);
