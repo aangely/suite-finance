d646181dee7b8efd3291cdd20a57e43edf4f176f
hengwangsay <heng.wang@crypto.com>
2022-10-05 11:22:04 +0800

    fix path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  assetPrefix: isProd ? "/suite-finance/" : undefined,
+  basePath: isProd ? "/suite-finance" : undefined,
+
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
